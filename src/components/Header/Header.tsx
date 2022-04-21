86b63e1604ddb83c41717f6c24054a27ded269da
Ammar <entar99ammar@gmail.com>
2022-04-21 09:13:35 +0200

    show and hide header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import classNames from "classnames";
 import Styles from "./Header.module.scss"
 import {Avatar} from "@mui/material";
@@ -20,9 +20,21 @@ const Header: React.FC = () => {
     const handleClose = (): void => {
         setAnchorEl(null);
     };
+    const [show, setShow] = useState<boolean>(true)
+    const transitionHeader = (): void => {
+        if (window.scrollY < 50) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+    useEffect((): ReturnType<any> => {
+        window.addEventListener("scroll", transitionHeader)
+        return () => window.removeEventListener
+    }, [])
     return (
         <div
-            className='fixed w-[100%] flex justify-between  top-0 p-[5px]  bg-[#0d0318] h-[40px] z-1'>
+            className={classNames([Styles.Header], 'fixed w-[100%] flex justify-between  top-0 p-[5px]   h-[40px] z-1', {'bg-[#0d0318]': show})}>
             <img
                 className=' left-0 cursor-pointer  ml-[10px]'
                 src={logo} alt=""/>
@@ -30,14 +42,14 @@ const Header: React.FC = () => {
             <p className=""/>
             <div className={classNames([Styles.Header_content], 'flex items-center mr-[10px]')}>
                 <SearchIcon className="hover:text-[#5418c5]" sx={{
-                    color: "#ffff",
+                    color: `${show ? 'white' : "#0d0318"}`,
                     width: 20,
                     height: 20,
                     cursor: "pointer",
                     marginRight: "20px"
 
                 }}/>
-                <ul className={classNames([Styles.Header_list], "flex list-none items-center text-xs mr-[30px]")}>
+                <ul className={classNames([Styles.Header_list], "flex list-none items-center text-xs mr-[30px]", {"text-[#0d0318]": !show})}>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/movies">Movies</Link></li>
                     <li><Link to="/about">About</Link></li>
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -3,6 +3,30 @@ import React from 'react';
 const HomeScreen: React.FC = () => {
     return (
         <div>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
+            <h1>Test</h1>
         </div>
     );
 };
