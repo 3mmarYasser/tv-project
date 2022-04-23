a02819f8481d870a9cb6b1e52f495d31471f41e6
Ammar <entar99ammar@gmail.com>
2022-04-23 10:10:45 +0200

    Banner Service

--- src/screens/HomeScreen/HomeScreen.service.tsx
+++ src/screens/HomeScreen/HomeScreen.service.tsx
@@ -1,4 +1,4 @@
-import React , {useEffect , useState} from "react"
+import  {useEffect , useState} from "react"
 import Trending from "../../api/requests/Trending";
 import {BannerResponse, TVShow} from "../../interfaces";
 
